46a7ec9bd415190c47ec25f0264ecc32cab9524b
Rob Rix <rob.rix@me.com>
2020-06-07 15:17:05 -0400

    Define sparse vectors.

--- src/Isometry/Voxel.hs
+++ src/Isometry/Voxel.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 module Isometry.Voxel
 ( B(..)
+, V(..)
 ) where
 
 -- | The shape of binary trees.
@@ -7,3 +10,9 @@ data B
   = Z
   | L
   | B B B
+
+-- | Sparse vectors.
+data V s a where
+  VZ :: V s a
+  VL :: V 'L a
+  VB :: V s1 a -> V s2 a -> V ('B s1 s2) a
