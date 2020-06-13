2181cd0549820e82b4f2651a8fccc5941ba8838d
Rob Rix <rob.rix@me.com>
2020-06-13 11:22:03 -0400

    Define an identity transform.

--- src/Geometry/Transform.hs
+++ src/Geometry/Transform.hs
@@ -12,6 +12,7 @@ module Geometry.Transform
 , mkRotation
 , apply
 , tmap
+, identity
 , (>>>)
 , (<<<)
 , (<*<)
@@ -28,7 +29,8 @@ import           Data.Kind (Type)
 import           Foreign.Storable
 import qualified GL.Type as GL
 import           GL.Uniform
-import           Linear.Exts
+import           Linear.Exts hiding (identity)
+import qualified Linear.Exts as L
 import           Prelude hiding ((.))
 import           Unit
 import           Unit.Algebra
@@ -41,17 +43,20 @@ deriving instance GL.Type (m (m c)) => GL.Type (Transform m c a b)
 deriving instance Uniform (m (m c)) => Uniform (Transform m c a b)
 
 instance (Num c, Additive m, Applicative m, Traversable m) => Category (Transform m c) where
-  id = Transform identity
+  id = identity
   Transform a . Transform b = Transform (a !*! b)
 
+identity :: (Applicative t, Traversable t, Num c) => Transform t c v u
+identity = Transform L.identity
+
 mkTranslation :: (Num c, Unit d u, Applicative m, R4 m, Representable m, Traversable m) => V3 (u c) -> Transform m c u u
-mkTranslation v = Transform (identity & translation .~ fmap prj v)
+mkTranslation v = Transform (L.identity & translation .~ fmap prj v)
 
 mkScale :: forall u v c du dv d' . (Num c, Unit du u, Unit dv v, Unit d' (Div u v)) => V3 (Div u v c) -> Transform V4 c v u
 mkScale v = Transform (scaled (point (prj <$> v)))
 
 mkRotation :: Num c => Quaternion (I c) -> Transform V4 c a a
-mkRotation q = Transform (identity !*! mkTransformation (coerce q) 0)
+mkRotation q = Transform (L.identity !*! mkTransformation (coerce q) 0)
 
 apply :: (Num c, Unit d a, Unit d b, Additive m, Foldable m) => Transform m c a b -> m (a c) -> m (b c)
 apply (Transform m) v = pure <$> (m !* fmap prj v)
