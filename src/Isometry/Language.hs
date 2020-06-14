cce2feb7961467ffbc244fa859c9a69aff5dd90d
Rob Rix <rob.rix@me.com>
2020-06-14 17:49:11 -0400

    Define a language with coordinates.

--- src/Isometry/Language.hs
+++ src/Isometry/Language.hs
@@ -1,6 +1,13 @@
 module Isometry.Language
-( Time(..)
+( Coords(..)
+, Time(..)
 ) where
+
+import Linear.V3
+import Unit.Length
+
+class Coords expr where
+  coords :: expr (V3 (Metres Double))
 
 class Time expr where
   time :: expr Double
