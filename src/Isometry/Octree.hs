3c6696ae643808edbe4fc09774712ed27c3d4424
Rob Rix <rob.rix@me.com>
2020-06-18 13:03:42 -0400

    :memo: dense unfoldings.

--- src/Isometry/Octree.hs
+++ src/Isometry/Octree.hs
@@ -102,9 +102,11 @@ instance (FoldableWithIndex (v Bit) f, FunctorWithIndex (v Bit) f, TraversableWi
   itraverse f (L a) = L <$> f (pure II) a
   itraverse f (B b) = B <$> itraverse (\ i -> itraverse (\ j -> f (toIndex <$> i <*> j))) b
 
+-- | Note that this instance can only express dense unfoldings.
 instance (UnfoldableWithIndex (v Bit) f, Applicative v) => UnfoldableWithIndex (v (Index 'S1)) (B 'S1 f) where
   iunfoldA f = L <$> f (pure II)
 
+-- | Note that this instance can only express dense unfoldings.
 instance (UnfoldableWithIndex (v Bit) f, Applicative v, UnfoldableWithIndex (v (Index s)) (B s f)) => UnfoldableWithIndex (v (Index ('S2x s))) (B ('S2x s) f) where
   iunfoldA f = B <$> iunfoldA (\ i -> iunfoldA (\ j -> f (toIndex <$> i <*> j)))
 
