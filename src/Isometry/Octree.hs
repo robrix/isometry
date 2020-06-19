929300a1eaeebe1499edb2024aa3296cd3353edc
Rob Rix <rob.rix@me.com>
2020-06-19 09:26:55 -0400

    Define a Bits instance for Bit.

--- src/Isometry/Octree.hs
+++ src/Isometry/Octree.hs
@@ -60,6 +60,7 @@ module Isometry.Octree
 import           Control.Carrier.State.Church
 import           Control.Lens (Lens', set)
 import           Control.Lens.Indexed hiding (Indexed(..))
+import           Data.Bits
 import           Data.Bool
 import           Data.Generics.Product.Fields
 import           Data.Proxy
@@ -93,6 +94,39 @@ toBit = bool I0 I1
 
 fromBit :: Bit -> Bool
 fromBit = (== I1)
+
+instance Bits Bit where
+  -- fixme: should we avoid matching on the rhs when possible for laziness?
+  I1 .&. I1 = I1
+  _  .&. _  = I0
+
+  I0 .|. I0 = I0
+  _  .|. _  = I1
+
+  a `xor` b = if a /= b then I1 else I0
+
+  complement I0 = I1
+  complement I1 = I0
+
+  shift x 0 = x
+  shift _ _ = I0
+
+  rotate x _ = x
+
+  bit 0 = I1
+  bit _ = I0
+
+  testBit x 0 = fromBit x
+  testBit _ _ = False
+
+  bitSizeMaybe _ = Just 1
+
+  bitSize _ = 1
+
+  isSigned _ = False
+
+  popCount I0 = 0
+  popCount I1 = 1
 
 fromIndex :: Index ('S2x i) -> (Bit, Index i)
 fromIndex (IB b i) = (b, i)
