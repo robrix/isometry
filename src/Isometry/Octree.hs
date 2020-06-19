79319de86421263493c1e9ee091e53f11b3a930f
Rob Rix <rob.rix@me.com>
2020-06-19 16:35:11 -0400

    Define synonyms for a bunch of shapes.

--- src/Isometry/Octree.hs
+++ src/Isometry/Octree.hs
@@ -17,6 +17,19 @@
 -- | Sparse vectors, matrices, and volumes, represented as perfectly balanced binary trees.
 module Isometry.Octree
 ( Shape(..)
+, S2
+, S4
+, S8
+, S16
+, S32
+, S64
+, S128
+, S256
+, S512
+, S1024
+, S2048
+, S4096
+, S8192
 , Size
 , Bit(..)
 , toBit
@@ -78,6 +91,20 @@ import           Linear.V3
 data Shape
   = S1         -- 1
   | S2x !Shape -- 2 * n
+
+type S2 = 'S2x 'S1
+type S4 = 'S2x S2
+type S8 = 'S2x S4
+type S16 = 'S2x S8
+type S32 = 'S2x S16
+type S64 = 'S2x S32
+type S128 = 'S2x S64
+type S256 = 'S2x S128
+type S512 = 'S2x S256
+type S1024 = 'S2x S512
+type S2048 = 'S2x S1024
+type S4096 = 'S2x S2048
+type S8192 = 'S2x S4096
 
 
 type family Size (b :: Shape) :: Nat where
