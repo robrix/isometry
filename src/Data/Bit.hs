59af2ff4c7381868dced4da81d4daeb043b4175d
Rob Rix <rob.rix@me.com>
2020-06-20 10:31:35 -0400

    Move Bit into its own module.

--- src/Data/Bit.hs
+++ src/Data/Bit.hs
@@ -1,2 +1,57 @@
 module Data.Bit
-() where
+( Bit(..)
+, toBit
+, fromBit
+) where
+
+import Data.Bits
+import Data.Bool (bool)
+
+data Bit
+  = I0
+  | I1
+  deriving (Enum, Eq, Ord, Show)
+
+toBit :: Bool -> Bit
+toBit = bool I0 I1
+
+fromBit :: Bit -> Bool
+fromBit = (== I1)
+
+instance Bits Bit where
+  -- fixme: should we avoid matching on the rhs when possible for laziness?
+  I1 .&. I1 = I1
+  _  .&. _  = I0
+
+  I0 .|. I0 = I0
+  _  .|. _  = I1
+
+  a `xor` b = if a /= b then I1 else I0
+
+  complement I0 = I1
+  complement I1 = I0
+
+  shift x 0 = x
+  shift _ _ = I0
+
+  rotate x _ = x
+
+  bit 0 = I1
+  bit _ = I0
+
+  testBit x 0 = fromBit x
+  testBit _ _ = False
+
+  bitSizeMaybe _ = Just 1
+
+  bitSize _ = 1
+
+  isSigned _ = False
+
+  popCount I0 = 0
+  popCount I1 = 1
+
+instance FiniteBits Bit where
+  finiteBitSize _ = 1
+  countTrailingZeros = countTrailingZeros . fromBit
+  countLeadingZeros = countLeadingZeros . fromBit
--- src/Data/Octree.hs
+++ src/Data/Octree.hs
@@ -31,9 +31,6 @@ module Data.Octree
 , S4096
 , S8192
 , Size
-, Bit(..)
-, toBit
-, fromBit
 , Index(..)
 , fromIndex
 , toFraction
@@ -73,8 +70,8 @@ module Data.Octree
 import           Control.Carrier.State.Church
 import           Control.Lens (Lens', set)
 import           Control.Lens.Indexed hiding (Indexed(..))
+import           Data.Bit
 import           Data.Bits
-import           Data.Bool
 import           Data.Generics.Product.Fields
 import           Data.Proxy
 import qualified Data.Vector as V
@@ -110,56 +107,6 @@ type S8192 = 'S2x S4096
 type family Size (b :: Shape) :: Nat where
   Size 'S1      = 1
   Size ('S2x l) = 2 * Size l
-
-
-data Bit
-  = I0
-  | I1
-  deriving (Enum, Eq, Ord, Show)
-
-toBit :: Bool -> Bit
-toBit = bool I0 I1
-
-fromBit :: Bit -> Bool
-fromBit = (== I1)
-
-instance Bits Bit where
-  -- fixme: should we avoid matching on the rhs when possible for laziness?
-  I1 .&. I1 = I1
-  _  .&. _  = I0
-
-  I0 .|. I0 = I0
-  _  .|. _  = I1
-
-  a `xor` b = if a /= b then I1 else I0
-
-  complement I0 = I1
-  complement I1 = I0
-
-  shift x 0 = x
-  shift _ _ = I0
-
-  rotate x _ = x
-
-  bit 0 = I1
-  bit _ = I0
-
-  testBit x 0 = fromBit x
-  testBit _ _ = False
-
-  bitSizeMaybe _ = Just 1
-
-  bitSize _ = 1
-
-  isSigned _ = False
-
-  popCount I0 = 0
-  popCount I1 = 1
-
-instance FiniteBits Bit where
-  finiteBitSize _ = 1
-  countTrailingZeros = countTrailingZeros . fromBit
-  countLeadingZeros = countLeadingZeros . fromBit
 
 
 data Index i where
