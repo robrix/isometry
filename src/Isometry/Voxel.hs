39b9d097066a61bd3e0057fbdf803a969b616f15
Rob Rix <rob.rix@me.com>
2020-06-20 10:28:06 -0400

    Define a Side datatype describing the faces of a voxel.

--- src/Isometry/Voxel.hs
+++ src/Isometry/Voxel.hs
@@ -2,8 +2,18 @@ module Isometry.Voxel
 ( Voxel(..)
 ) where
 
+import           Data.Ix
 import qualified UI.Colour as UI
 
 -- FIXME: indicate which sides are present
 
 newtype Voxel = Voxel { colour :: UI.Colour Float }
+
+data Side
+  = L
+  | R
+  | B
+  | T
+  | F
+  | N
+  deriving (Enum, Eq, Ix, Ord, Show)
