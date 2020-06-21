b6fb87a14e4c28f8c0f734103d2cbc18cadc6408
Rob Rix <rob.rix@me.com>
2020-06-21 12:26:07 -0400

    Define a FoldableWithIndex instance for :.:.

--- src/Data/Functor/C.hs
+++ src/Data/Functor/C.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Data.Functor.C
 ( (:.:)(..)
 ) where
 
 import Control.Applicative
+import Control.Lens.Indexed
 
 newtype (f :.: g) a = C { getC :: f (g a) }
   deriving (Eq, Foldable, Functor, Monoid, Ord, Semigroup, Show, Traversable)
@@ -18,3 +21,6 @@ instance (Applicative f, Applicative g) => Applicative (f :.: g) where
 
   C f <*> C a = C $ liftA2 (<*>) f a
   {-# INLINE (<*>) #-}
+
+instance (FoldableWithIndex i f, FoldableWithIndex j g) => FoldableWithIndex (i, j) (f :.: g) where
+  ifoldMap f = ifoldMap (ifoldMap . fmap f . (,)) . getC
