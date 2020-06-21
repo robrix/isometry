768d9538a7206affbfd6fb0d05304de2f8061ff2
Rob Rix <rob.rix@me.com>
2020-06-21 16:46:33 -0400

    Specialize the {Foldable,Functor,Traversable}WithIndex instances for :.: to basis projections.

--- src/Data/Functor/C.hs
+++ src/Data/Functor/C.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
@@ -11,6 +12,7 @@ module Data.Functor.C
 import Control.Applicative
 import Control.Lens (Iso', iso)
 import Control.Lens.Indexed
+import Linear.Vector
 
 newtype (f :.: g) a = C { getC :: f (g a) }
   deriving (Eq, Foldable, Functor, Monoid, Ord, Semigroup, Show, Traversable)
@@ -27,14 +29,14 @@ instance (Applicative f, Applicative g) => Applicative (f :.: g) where
   C f <*> C a = C $ liftA2 (<*>) f a
   {-# INLINE (<*>) #-}
 
-instance (FoldableWithIndex i f, FoldableWithIndex j g) => FoldableWithIndex (i, j) (f :.: g) where
-  ifoldMap f = ifoldMap (ifoldMap . fmap f . (,)) . getC
+instance (FoldableWithIndex (E f) f, FoldableWithIndex (E g) g) => FoldableWithIndex (E (f :.: g)) (f :.: g) where
+  ifoldMap f = ifoldMap (\ i -> ifoldMap (\ j -> f (E (compose_.el i.el j)))) . getC
   {-# INLINABLE ifoldMap #-}
 
-instance (FunctorWithIndex i f, FunctorWithIndex j g) => FunctorWithIndex (i, j) (f :.: g) where
-  imap f = C . imap (imap . fmap f . (,)) . getC
+instance (FunctorWithIndex (E f) f, FunctorWithIndex (E g) g) => FunctorWithIndex (E (f :.: g)) (f :.: g) where
+  imap f = C . imap (\ i -> imap (\ j -> f (E (compose_.el i.el j)))) . getC
   {-# INLINABLE imap #-}
 
-instance (TraversableWithIndex i f, TraversableWithIndex j g) => TraversableWithIndex (i, j) (f :.: g) where
-  itraverse f = fmap C . itraverse (itraverse . fmap f . (,)) . getC
+instance (TraversableWithIndex (E f) f, TraversableWithIndex (E g) g) => TraversableWithIndex (E (f :.: g)) (f :.: g) where
+  itraverse f = fmap C . itraverse (\ i -> itraverse (\ j -> f (E (compose_.el i.el j)))) . getC
   {-# INLINABLE itraverse #-}
