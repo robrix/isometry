37e82e1c5fb1cd59459c2cdf626ee5e107845c1b
Rob Rix <rob.rix@me.com>
2020-06-27 16:51:05 -0400

    Define Octree in terms of Oct.

--- src/Data/Bin/Octree.hs
+++ src/Data/Bin/Octree.hs
@@ -12,20 +12,18 @@ module Data.Bin.Octree
 ) where
 
 import Control.Lens.Indexed
-import Data.Bin.Bit
 import Data.Bin.Index
+import Data.Bin.Oct hiding (Octree)
 import Data.Bin.Shape
-import Data.Bin.Tree (SparseUnfoldableWithIndex(..))
+import Data.Bin.Tree (SparseUnfoldableWithIndex(..), UnfoldableWithIndex(..))
+import Data.Monoid (Sum(..))
 import Linear.V3
 
 data Octree s a where
   E :: Octree s a
   L :: !a -> Octree 'S1 a
   B :: {-# UNPACK #-} !Int
-    -> !(Octree s a) -> !(Octree s a)
-    -> !(Octree s a) -> !(Octree s a)
-    -> !(Octree s a) -> !(Octree s a)
-    -> !(Octree s a) -> !(Octree s a)
+    -> {-# UNPACK #-} !(Oct (Octree s a))
     -> Octree ('S2x s) a
 
 instance Foldable (Octree s) where
@@ -35,59 +33,29 @@ instance Foldable (Octree s) where
     go = \case
       E   -> mempty
       L a -> f a
-      B _ lbf rbf
-          ltf rtf
-          lbn rbn
-          ltn rtn
-        -> go lbf <> go rbf
-        <> go ltf <> go rtf
-        <> go lbn <> go rbn
-        <> go ltn <> go rtn
+      B _ b -> foldMap go b
 
   length = \case
-    E   -> 0
-    L _ -> 1
-    B l _ _ _ _ _ _ _ _ -> l
+    E     -> 0
+    L   _ -> 1
+    B l _ -> l
 
 deriving instance Functor     (Octree s)
 deriving instance Traversable (Octree s)
 
 instance FoldableWithIndex (V3 (Index s)) (Octree s) where
   ifoldMap f = \case
-    E   -> mempty
-    L a -> f (pure IL) a
-    B _ lbf rbf
-        ltf rtf
-        lbn rbn
-        ltn rtn
-      -> ifoldMap (f . (IB <$> V3 I0 I0 I0 <*>)) lbf <> ifoldMap (f . (IB <$> V3 I1 I0 I0 <*>)) rbf
-      <> ifoldMap (f . (IB <$> V3 I0 I1 I0 <*>)) ltf <> ifoldMap (f . (IB <$> V3 I1 I1 I0 <*>)) rtf
-      <> ifoldMap (f . (IB <$> V3 I0 I0 I1 <*>)) lbn <> ifoldMap (f . (IB <$> V3 I1 I0 I1 <*>)) rbn
-      <> ifoldMap (f . (IB <$> V3 I0 I1 I1 <*>)) ltn <> ifoldMap (f . (IB <$> V3 I1 I1 I1 <*>)) rtn
+    E     -> mempty
+    L   a -> f (pure IL) a
+    B _ b -> ifoldMap (\ i -> ifoldMap (f . (IB <$> i <*>))) b
 
 instance SparseUnfoldableWithIndex V3 (Index 'S1) (Octree 'S1) where
   iunfoldSparseM _ leaf = L <$> leaf (pure IL)
 
 instance SparseUnfoldableWithIndex V3 (Index s) (Octree s) => SparseUnfoldableWithIndex V3 (Index ('S2x s)) (Octree ('S2x s)) where
-  iunfoldSparseM branch leaf = makeB
-    <$> go (V3 I0 I0 I0) <*> go (V3 I1 I0 I0)
-    <*> go (V3 I0 I1 I0) <*> go (V3 I1 I1 I0)
-    <*> go (V3 I0 I0 I1) <*> go (V3 I1 I0 I1)
-    <*> go (V3 I0 I1 I1) <*> go (V3 I1 I1 I1)
+  iunfoldSparseM branch leaf = makeB <$> iunfoldA go
     where
     go i = branch i >>= \ b -> if b then iunfoldSparseM branch (leaf . (IB <$> i <*>)) else pure E
 
-makeB :: Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree ('S2x s) a
-makeB
-  lbf rbf
-  ltf rtf
-  lbn rbn
-  ltn rtn
-  = B (length lbf + length rbf
-    +  length ltf + length rtf
-    +  length lbn + length rbn
-    +  length ltn + length rtn)
-    lbf rbf
-    ltf rtf
-    lbn rbn
-    ltn rtn
+makeB :: Oct (Octree s a) -> Octree ('S2x s) a
+makeB o = B (getSum (foldMap (Sum . length) o)) o
