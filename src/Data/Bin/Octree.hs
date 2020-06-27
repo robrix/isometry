478aa341cfc3ae4c8139180a730a904c20a448ce
Rob Rix <rob.rix@me.com>
2020-06-27 11:50:52 -0400

    Octree is strict in branches.

--- src/Data/Bin/Octree.hs
+++ src/Data/Bin/Octree.hs
@@ -9,8 +9,8 @@ import Data.Bin.Shape
 data Octree s a where
   E :: Octree s a
   L :: !a -> Octree 'S1 a
-  B :: Octree s a -> Octree s a
-    -> Octree s a -> Octree s a
-    -> Octree s a -> Octree s a
-    -> Octree s a -> Octree s a
+  B :: !(Octree s a) -> !(Octree s a)
+    -> !(Octree s a) -> !(Octree s a)
+    -> !(Octree s a) -> !(Octree s a)
+    -> !(Octree s a) -> !(Octree s a)
     -> Octree ('S2x s) a
