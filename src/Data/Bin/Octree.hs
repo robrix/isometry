e13074f5ad0a8c115425076d2f9d387162a44405
Rob Rix <rob.rix@me.com>
2020-06-27 11:49:21 -0400

    Define an Octree datatype.

--- src/Data/Bin/Octree.hs
+++ src/Data/Bin/Octree.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 module Data.Bin.Octree
-() where
+( Octree(..)
+) where
+
+import Data.Bin.Shape
+
+data Octree s a where
+  E :: Octree s a
+  L :: !a -> Octree 'S1 a
+  B :: Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
+    -> Octree ('S2x s) a
