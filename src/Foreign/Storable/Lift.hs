ef9ea12e3b2c40cab09db223647ab8aa266ef5c7
Rob Rix <rob.rix@me.com>
2020-06-28 16:55:23 -0400

    Define liftings of peek & poke.

--- src/Foreign/Storable/Lift.hs
+++ src/Foreign/Storable/Lift.hs
@@ -1,2 +1,15 @@
 module Foreign.Storable.Lift
-() where
+( S.Storable(S.sizeOf, S.alignment)
+, peek
+, poke
+) where
+
+import Foreign.Ptr
+import qualified Foreign.Storable as S
+import Control.Effect.Lift
+
+peek :: (Has (Lift IO) sig m, S.Storable a) => Ptr a -> m a
+peek = sendIO . S.peek
+
+poke :: (Has (Lift IO) sig m, S.Storable a) => Ptr a -> a -> m ()
+poke p = sendIO . S.poke p
