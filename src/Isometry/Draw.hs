4456099de5ab9c4d06d3e2f7bdbbe4a4d3b124f6
Rob Rix <rob.rix@me.com>
2020-06-28 19:28:29 -0400

    Set the clear colour using setClearColour.

--- src/Isometry/Draw.hs
+++ src/Isometry/Draw.hs
@@ -136,7 +136,7 @@ frame = timed $ do
 
     glDepthMask GL_TRUE
 
-    glClearColor 0 0 0 0
+    UI.setClearColour UI.black
     glClear $ GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT
 
     glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
