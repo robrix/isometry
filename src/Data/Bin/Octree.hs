79f23f0273e5236e7c010c7d599ba336acfa074c
Rob Rix <rob.rix@me.com>
2020-06-28 14:52:26 -0400

    :fire: the distinct Oct type.

--- src/Data/Bin/Octree.hs
+++ src/Data/Bin/Octree.hs
@@ -24,7 +24,10 @@ data Octree s a where
   E :: Octree s a
   L :: !a -> Octree 'S1 a
   B :: {-# UNPACK #-} !Int
-    -> {-# UNPACK #-} !(Oct s a)
+    -> Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
+    -> Octree s a -> Octree s a
     -> Octree ('S2x s) a
 
 instance Foldable (Octree s) where
@@ -32,15 +35,15 @@ instance Foldable (Octree s) where
     where
     go :: Octree s' a -> m
     go = \case
-      E     -> mempty
-      L   a -> f a
-      B _ b -> foldMap f b
+      E   -> mempty
+      L a -> f a
+      B _ lbf rbf ltf rtf lbn rbn ltn rtn -> foldMap f lbf <> foldMap f rbf <> foldMap f ltf <> foldMap f rtf <> foldMap f lbn <> foldMap f rbn <> foldMap f ltn <> foldMap f rtn
   {-# INLINABLE foldMap #-}
 
   length = \case
-    E     -> 0
-    L   _ -> 1
-    B l _ -> l
+    E                   -> 0
+    L _                 -> 1
+    B l _ _ _ _ _ _ _ _ -> l
   {-# INLINABLE length #-}
 
 deriving instance Functor     (Octree s)
@@ -50,7 +53,13 @@ instance FoldableWithIndex (V3 (Index s)) (Octree s) where
   ifoldMap f = \case
     E     -> mempty
     L   a -> f (pure il) a
-    B _ b -> ifoldMap f b
+    B _ lbf rbf ltf rtf lbn rbn ltn rtn
+      -> go B0 B0 B0 lbf <> go B1 B0 B0 rbf
+      <> go B0 B1 B0 ltf <> go B1 B1 B0 rtf
+      <> go B0 B0 B1 lbn <> go B1 B0 B1 rbn
+      <> go B0 B1 B1 ltn <> go B1 B1 B1 rtn
+      where
+      go x y z = ifoldMap (f . (ib <$> V3 x y z <*>))
   {-# INLINABLE ifoldMap #-}
 
 instance SparseUnfoldableWithIndex V3 (Index 'S1) (Octree 'S1) where
@@ -58,7 +67,13 @@ instance SparseUnfoldableWithIndex V3 (Index 'S1) (Octree 'S1) where
   {-# INLINABLE iunfoldSparseM #-}
 
 instance SparseUnfoldableWithIndex V3 (Index s) (Octree s) => SparseUnfoldableWithIndex V3 (Index ('S2x s)) (Octree ('S2x s)) where
-  iunfoldSparseM branch leaf = makeB <$> iunfoldSparseM branch leaf
+  iunfoldSparseM branch leaf = makeB
+    <$> go (V3 B0 B0 B0) <*> go (V3 B1 B0 B0)
+    <*> go (V3 B0 B1 B0) <*> go (V3 B1 B1 B0)
+    <*> go (V3 B0 B0 B1) <*> go (V3 B1 B0 B1)
+    <*> go (V3 B0 B1 B1) <*> go (V3 B1 B1 B1)
+    where
+    go i = branch i >>= \ b -> if b then iunfoldSparseM branch (leaf . (ib <$> i <*>)) else pure E
   {-# INLINABLE iunfoldSparseM #-}
 
 instance Applicative (Octree 'S1) where
@@ -68,50 +83,12 @@ instance Applicative (Octree 'S1) where
   L   f <*> a = fmap f a
 
 instance Applicative (Octree s) => Applicative (Octree ('S2x s)) where
-  pure = makeB . pure
+  pure a = makeB (pure a) (pure a) (pure a) (pure a) (pure a) (pure a) (pure a) (pure a)
 
   E     <*> _     = E
   _     <*> E     = E
-  B _ f <*> B _ a = makeB (f <*> a)
+  B _ f1 f2 f3 f4 f5 f6 f7 f8 <*> B _ a1 a2 a3 a4 a5 a6 a7 a8 = makeB (f1 <*> a1) (f2 <*> a2) (f3 <*> a3) (f4 <*> a4) (f5 <*> a5) (f6 <*> a6) (f7 <*> a7) (f8 <*> a8)
 
-makeB :: Oct s a -> Octree ('S2x s) a
-makeB o = B (length o) o
+makeB :: Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree s a -> Octree ('S2x s) a
+makeB o1 o2 o3 o4 o5 o6 o7 o8 = B (length o1 + length o2 + length o3 + length o4 + length o5 + length o6 + length o7 + length o8) o1 o2 o3 o4 o5 o6 o7 o8
 {-# INLINABLE makeB #-}
-
-
-data Oct s a = Oct { lbf, rbf, ltf, rtf, lbn, rbn, ltn, rtn :: Octree s a }
-  deriving (Functor, Traversable)
-
-instance Foldable (Oct s) where
-  foldMap f Oct{ lbf, rbf, ltf, rtf, lbn, rbn, ltn, rtn }
-    =  foldMap f lbf <> foldMap f rbf
-    <> foldMap f ltf <> foldMap f rtf
-    <> foldMap f lbn <> foldMap f rbn
-    <> foldMap f ltn <> foldMap f rtn
-  {-# INLINABLE foldMap #-}
-
-  length Oct{ lbf, rbf, ltf, rtf, lbn, rbn, ltn, rtn } = length lbf + length rbf + length ltf + length rtf + length lbn + length rbn + length ltn + length rtn
-  {-# INLINABLE length #-}
-
-instance FoldableWithIndex (V3 (Index ('S2x s))) (Oct s) where
-  ifoldMap f Oct{ lbf, rbf, ltf, rtf, lbn, rbn, ltn, rtn }
-    =  go B0 B0 B0 lbf <> go B1 B0 B0 rbf
-    <> go B0 B1 B0 ltf <> go B1 B1 B0 rtf
-    <> go B0 B0 B1 lbn <> go B1 B0 B1 rbn
-    <> go B0 B1 B1 ltn <> go B1 B1 B1 rtn
-    where
-    go x y z = ifoldMap (f . (ib <$> V3 x y z <*>))
-
-instance SparseUnfoldableWithIndex V3 (Index s) (Octree s) => SparseUnfoldableWithIndex V3 (Index ('S2x s)) (Oct s) where
-  iunfoldSparseM branch leaf = Oct
-    <$> go (V3 B0 B0 B0) <*> go (V3 B1 B0 B0)
-    <*> go (V3 B0 B1 B0) <*> go (V3 B1 B1 B0)
-    <*> go (V3 B0 B0 B1) <*> go (V3 B1 B0 B1)
-    <*> go (V3 B0 B1 B1) <*> go (V3 B1 B1 B1)
-    where
-    go i = branch i >>= \ b -> if b then iunfoldSparseM branch (leaf . (ib <$> i <*>)) else pure E
-
-instance Applicative (Octree s) => Applicative (Oct s) where
-  pure a = Oct (pure a) (pure a) (pure a) (pure a) (pure a) (pure a) (pure a) (pure a)
-
-  Oct f1 f2 f3 f4 f5 f6 f7 f8 <*> Oct a1 a2 a3 a4 a5 a6 a7 a8 = Oct (f1 <*> a1) (f2 <*> a2) (f3 <*> a3) (f4 <*> a4) (f5 <*> a5) (f6 <*> a6) (f7 <*> a7) (f8 <*> a8)
