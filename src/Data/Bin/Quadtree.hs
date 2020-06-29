726d31f2059f3d17f7df45afe348c7a5fccc1bdc
Rob Rix <rob.rix@me.com>
2020-06-29 19:42:42 -0400

    Define a FoldableWithIndex instance for Quadtree.

--- src/Data/Bin/Quadtree.hs
+++ src/Data/Bin/Quadtree.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Data.Bin.Quadtree
 ( Quadtree(..)
 ) where
 
+import Control.Lens.Indexed
+import Data.Bin.Bit
+import Data.Bin.Index
 import Data.Bin.Shape
+import Linear.V2
 
 data Quadtree s a where
   E :: Quadtree s a
@@ -36,3 +42,14 @@ instance Foldable (Quadtree s) where
 
 deriving instance Functor     (Quadtree s)
 deriving instance Traversable (Quadtree s)
+
+instance FoldableWithIndex (V2 (Index s)) (Quadtree s) where
+  ifoldMap f = \case
+    E     -> mempty
+    L   a -> f (pure il) a
+    B _ lb rb lt rt
+      -> go (V2 B0 B0) lb <> go (V2 B1 B0) rb
+      <> go (V2 B0 B1) lt <> go (V2 B1 B1) rt
+      where
+      go b = ifoldMap (f . (ib <$> b <*>))
+  {-# INLINABLE ifoldMap #-}
