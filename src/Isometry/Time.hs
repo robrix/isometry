3cfd4181599eae10c08c6cca28c34a93f97f4b26
Rob Rix <rob.rix@me.com>
2020-06-29 09:06:23 -0400

    Inherit timings from fused-effects-profile.

--- src/Isometry/Time.hs
+++ src/Isometry/Time.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Isometry.Time
 ( Instant(..)
 , Duration(..)
@@ -10,21 +9,8 @@ module Isometry.Time
 import Control.Carrier.Reader
 import Control.Effect.Lift
 import Control.Effect.State
-import Data.Fixed
-import System.CPUTime
+import Data.Timing
 import Unit.Time
-
-newtype Instant = Instant { getInstant :: Fixed E12 }
-  deriving (Eq, Fractional, Num, Ord, Real, Show)
-
-newtype Duration = Duration { getDuration :: Fixed E12 }
-  deriving (Eq, Fractional, Num, Ord, Real, Show)
-
-now :: Has (Lift IO) sig m => m Instant
-now = Instant . MkFixed <$> sendM getCPUTime
-
-since :: Has (Lift IO) sig m => Instant -> m Duration
-since (Instant t) = Duration . (t -) . getInstant <$> now
 
 timed
   :: ( Has (Lift IO) sig m
@@ -33,6 +19,6 @@ timed
   => ReaderC (Seconds Double) m a
   -> m a
 timed m = do
-  dt <- fmap realToFrac . since =<< get
+  dt <- fmap realToFrac . since <$> get <*> now
   put =<< now
   runReader dt m
