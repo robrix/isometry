6f0d999396d3845f4d8707cc3e637de0f6b83533
Rob Rix <rob.rix@me.com>
2020-06-29 19:14:21 -0400

    Define a Bintree datatype.

--- src/Data/Bin/Bintree.hs
+++ src/Data/Bin/Bintree.hs
@@ -1,2 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 module Data.Bin.Bintree
-() where
+( Bintree(..)
+) where
+
+import Data.Bin.Shape
+
+data Bintree s a where
+  E :: Bintree s a
+  L :: !a -> Bintree 'S1 a
+  B :: {-# UNPACK #-} !Int
+    -> !(Bintree s a) -> !(Bintree s a)
+    -> Bintree ('S2x s) a
