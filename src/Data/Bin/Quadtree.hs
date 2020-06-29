389ea2ba7b61f66c6576a01f75da5869ff704c80
Rob Rix <rob.rix@me.com>
2020-06-29 19:13:50 -0400

    Define a Quadtree datatype.

--- src/Data/Bin/Quadtree.hs
+++ src/Data/Bin/Quadtree.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 module Data.Bin.Quadtree
-() where
+( Quadtree(..)
+) where
+
+import Data.Bin.Shape
+
+data Quadtree s a where
+  E :: Quadtree s a
+  L :: !a -> Quadtree 'S1 a
+  B :: {-# UNPACK #-} !Int
+    -> !(Quadtree s a) -> !(Quadtree s a)
+    -> !(Quadtree s a) -> !(Quadtree s a)
+    -> Quadtree ('S2x s) a
