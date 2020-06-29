dce9f6e75926074c5331c02e73d15bedcd958fa7
Rob Rix <rob.rix@me.com>
2020-06-29 19:38:54 -0400

    Define a Foldable instance for Quadtree.

--- src/Data/Bin/Quadtree.hs
+++ src/Data/Bin/Quadtree.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Bin.Quadtree
 ( Quadtree(..)
 ) where
@@ -13,3 +15,19 @@ data Quadtree s a where
     -> !(Quadtree s a) -> !(Quadtree s a)
     -> !(Quadtree s a) -> !(Quadtree s a)
     -> Quadtree ('S2x s) a
+
+instance Foldable (Quadtree s) where
+  foldMap (f :: a -> m) = go
+    where
+    go :: Quadtree s' a -> m
+    go = \case
+      E       -> mempty
+      L a     -> f a
+      B _ lb rb lt rt -> go lb <> go rb <> go lt <> go rt
+  {-# INLINABLE foldMap #-}
+
+  length = \case
+    E           -> 0
+    L _         -> 1
+    B l _ _ _ _ -> l
+  {-# INLINABLE length #-}
