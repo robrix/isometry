12bc1a7411b2fdd73780156bc3889e99725328f7
Rob Rix <rob.rix@me.com>
2020-07-02 18:24:50 -0400

    Define insertion.

--- src/Data/IntervalSet.hs
+++ src/Data/IntervalSet.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE LambdaCase #-}
 module Data.IntervalSet
 ( IntervalSet(..)
 , empty
 , singleton
+, insert
   -- * Re-exports
 , Interval(..)
 ) where
@@ -18,3 +20,15 @@ empty = Empty
 
 singleton :: Interval I a -> IntervalSet a
 singleton a = Branch empty a empty
+
+
+insert :: Ord a => Interval I a -> IntervalSet a -> IntervalSet a
+insert i = \case
+  Empty -> singleton i
+  Branch l i' g
+    | sup i < inf i' -> Branch (insert i l) i' g
+    | inf i < sup i' -> Branch l i' (insert i g)
+    | otherwise      -> mkBranch l (i `union` i') g
+
+mkBranch :: IntervalSet a -> Interval I a -> IntervalSet a -> IntervalSet a
+mkBranch = Branch
