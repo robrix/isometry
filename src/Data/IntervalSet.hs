39c2061c2eedd4db4fefa2c83056afc29feb0f10
Rob Rix <rob.rix@me.com>
2020-07-02 18:17:15 -0400

    Define the empty interval set.

--- src/Data/IntervalSet.hs
+++ src/Data/IntervalSet.hs
@@ -1,5 +1,6 @@
 module Data.IntervalSet
 ( IntervalSet(..)
+, empty
   -- * Re-exports
 , Interval(..)
 ) where
@@ -10,3 +11,6 @@ import Data.Functor.Interval
 data IntervalSet a
   = Empty
   | Branch (IntervalSet a) (Interval I a) (IntervalSet a)
+
+empty :: IntervalSet a
+empty = Empty
