ce14ff4c0df4038f65923d977de8fa19be0bbf08
Rob Rix <rob.rix@me.com>
2020-07-04 13:21:39 -0400

    Cache the bounds on branches.

--- src/Data/IntervalSet.hs
+++ src/Data/IntervalSet.hs
@@ -11,31 +11,30 @@ module Data.IntervalSet
 
 import Data.Functor.I
 import Data.Functor.Interval
-import Data.Maybe (fromMaybe)
 
 data IntervalSet a
   = Empty
-  | Branch (IntervalSet a) (Interval I a) (IntervalSet a)
+  | Branch (Interval I a) (IntervalSet a) (Interval I a) (IntervalSet a)
 
 empty :: IntervalSet a
 empty = Empty
 
 singleton :: Interval I a -> IntervalSet a
-singleton a = Branch empty a empty
+singleton i = Branch i empty i empty
 
 
-bounds :: Ord a => IntervalSet a -> Maybe (Interval I a)
-bounds Empty          = Nothing
-bounds (Branch l i g) = Just (fromMaybe i (bounds l) `union` fromMaybe i (bounds g))
+bounds :: IntervalSet a -> Maybe (Interval I a)
+bounds Empty            = Nothing
+bounds (Branch b _ _ _) = Just b
 
 
 insert :: Ord a => Interval I a -> IntervalSet a -> IntervalSet a
 insert i = \case
   Empty -> singleton i
-  Branch l i' g
-    | sup i < inf i' -> Branch (insert i l) i' g
-    | inf i < sup i' -> Branch l i' (insert i g)
-    | otherwise      -> merge l (i `union` i') g
+  Branch b l i' g
+    | sup i < inf i' -> Branch (i `union` b) (insert i l) i' g
+    | inf i < sup i' -> Branch (i `union` b) l i' (insert i g)
+    | otherwise      -> merge (i `union` b) l (i `union` i') g
 
-merge :: IntervalSet a -> Interval I a -> IntervalSet a -> IntervalSet a
+merge :: Interval I a -> IntervalSet a -> Interval I a -> IntervalSet a -> IntervalSet a
 merge = Branch
