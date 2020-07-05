ae77ed8fdce2ddff248f5fde92e0ffaace20dbe8
Rob Rix <rob.rix@me.com>
2020-07-05 12:44:16 -0400

    Prepend intervals onto the remainder of the set.

--- src/Data/IntervalSet.hs
+++ src/Data/IntervalSet.hs
@@ -10,12 +10,13 @@ module Data.IntervalSet
 , Interval(..)
 ) where
 
+import           Data.FingerTree hiding (empty, null, singleton)
 import qualified Data.FingerTree as F
 import           Data.Functor.I
 import           Data.Functor.Interval
 import           Prelude hiding (null)
 
-newtype IntervalSet a = IntervalSet { getIntervalSet :: F.FingerTree (Maybe (Interval I a)) (Interval I a) }
+newtype IntervalSet a = IntervalSet { getIntervalSet :: FingerTree (Maybe (Interval I a)) (Interval I a) }
   deriving (Eq, Ord, Show)
 
 empty :: Ord a => IntervalSet a
@@ -26,20 +27,21 @@ singleton i = IntervalSet $ F.singleton i
 
 
 bounds :: Ord a => IntervalSet a -> Maybe (Interval I a)
-bounds = F.measure . getIntervalSet
+bounds = measure . getIntervalSet
 
 null :: IntervalSet a -> Bool
 null = F.null . getIntervalSet
 
 
 insert :: Ord a => Interval I a -> IntervalSet a -> IntervalSet a
-insert i set
-  | null set  = singleton i
-  | otherwise = IntervalSet (lt F.>< go gt)
+insert new = IntervalSet . go . getIntervalSet
   where
-  (lt, gt) = F.split (\case
-    Just i' -> sup i' < inf i
-    Nothing -> False) (getIntervalSet set)
   go set
-    | F.null set = F.singleton i
-    | otherwise  = i F.<| set
+    | F.null set      = F.singleton new
+    | Just i <- measure set
+    , inf i < sup new = new <| set
+    | otherwise       = lt >< go gt
+    where
+    (lt, gt) = split (\case
+      Just i -> sup i < inf new
+      Nothing -> False) set
