ea8e34b85a6d7b663fa6b23a0d20118131476d72
Rob Rix <rob.rix@me.com>
2020-07-05 15:12:48 -0400

    Test that empty is null.

--- test/Data/IntervalSet/Test.hs
+++ test/Data/IntervalSet/Test.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
 module Data.IntervalSet.Test
 ( tests
 ) where
 
+import Data.IntervalSet as I
+import Hedgehog
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "IntervalSet"
-  []
+  [ testProperty "empty is null" . property $ I.null (empty @Int) === True
+  ]
