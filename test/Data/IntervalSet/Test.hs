4f6cbf20e2ab9e8ab9be0e8409772d6f2ec4ff53
Rob Rix <rob.rix@me.com>
2020-07-05 15:18:03 -0400

    Test that insertion is idempotent.

--- test/Data/IntervalSet/Test.hs
+++ test/Data/IntervalSet/Test.hs
@@ -3,12 +3,25 @@ module Data.IntervalSet.Test
 ( tests
 ) where
 
+import Data.Functor.I
+import Data.Functor.Interval
 import Data.IntervalSet as I
 import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
 tests :: TestTree
 tests = testGroup "IntervalSet"
   [ testProperty "empty is null" . property $ I.null (empty @Int) === True
+  , testGroup "insert"
+    [ testProperty "idempotence" . property $ do
+      i <- forAll (interval (Gen.int (Range.linear 0 100)))
+      let s = insert i empty
+      insert i s === s
+    ]
   ]
+
+interval :: MonadGen m => m a -> m (Interval I a)
+interval p = (...) <$> p <*> p
