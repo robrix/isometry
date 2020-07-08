141f35ffca5f986b7c6cce9b904d5ddf7bc8da89
Rob Rix <rob.rix@me.com>
2020-07-07 20:49:40 -0400

    Go back to a manual definition of IntervalSet.

--- src/Data/IntervalSet.hs
+++ src/Data/IntervalSet.hs
@@ -1,50 +1,44 @@
+{-# LANGUAGE LambdaCase #-}
 module Data.IntervalSet
-( IntervalSet(IntervalSet)
+( IntervalSet()
 , empty
 , singleton
 , bounds
 , null
 , insert
-, split
-, before
-, after
   -- * Re-exports
 , Interval(..)
 ) where
 
-import           Data.FingerTree hiding (empty, null, singleton, split)
-import qualified Data.FingerTree as F
-import           Data.Functor.I
-import           Data.Functor.Interval
-import           Prelude hiding (null)
+import Data.Functor.I
+import Data.Functor.Interval
+import Prelude hiding (null)
 
-newtype IntervalSet a = IntervalSet { getIntervalSet :: FingerTree (Maybe (Interval I a)) (Interval I a) }
+data IntervalSet a
+  = Empty
+  | Node (Interval I a) (IntervalSet a) (Interval I a) (IntervalSet a)
   deriving (Eq, Ord, Show)
 
-empty :: Ord a => IntervalSet a
-empty = IntervalSet F.empty
+empty :: IntervalSet a
+empty = Empty
 
-singleton :: Ord a => Interval I a -> IntervalSet a
-singleton i = IntervalSet $ F.singleton i
+singleton :: Interval I a -> IntervalSet a
+singleton i = Node i Empty i Empty
 
 
-bounds :: Ord a => IntervalSet a -> Maybe (Interval I a)
-bounds = measure . getIntervalSet
+bounds :: IntervalSet a -> Maybe (Interval I a)
+bounds Empty          = Nothing
+bounds (Node b _ _ _) = Just b
 
 null :: IntervalSet a -> Bool
-null = F.null . getIntervalSet
+null Empty = True
+null _     = False
 
 
 insert :: Ord a => Interval I a -> IntervalSet a -> IntervalSet a
-insert new (IntervalSet set) = IntervalSet $ case F.split (before new) set of
-  (lt, rest) -> lt >< case F.split (after new) rest of
-    (mid, gt) -> maybe new (union new) (measure mid) <| gt
-
-split :: Ord a => (Maybe (Interval I a) -> Bool) -> IntervalSet a -> (IntervalSet a, IntervalSet a)
-split p (IntervalSet set) = let (lt, gt) = F.split p set in (IntervalSet lt, IntervalSet gt)
-
-before :: Ord a => Interval I a -> Maybe (Interval I a) -> Bool
-before a = maybe False (\ b -> inf a <= sup b)
-
-after :: Ord a => Interval I a -> Maybe (Interval I a) -> Bool
-after a = maybe False (\ b -> sup a < sup b)
+insert new = \case
+  Empty -> singleton new
+  Node b l i r
+    | b `isSubintervalOf` new -> singleton new
+    | sup new < inf b -> Node (new <> b) Empty new (Node b l i r)
+    | sup b < inf new -> Node (b <> new) (Node b l i r) new Empty
