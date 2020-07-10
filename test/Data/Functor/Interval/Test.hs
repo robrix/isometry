9ebeef47955572737db8e1cf1050efd40dc55209
Rob Rix <rob.rix@me.com>
2020-07-10 07:05:51 -0400

    Test that union is commutative.

--- test/Data/Functor/Interval/Test.hs
+++ test/Data/Functor/Interval/Test.hs
@@ -31,6 +31,9 @@ tests = map checkParallel
     , ("associativity", property $ do
       (i1, i2, i3) <- forAll ((,,) <$> gi <*> gi <*> gi)
       (i1 `union` i2) `union` i3 === i1 `union` (i2 `union` i3))
+    , ("commutativity", property $ do
+      (i1, i2) <- forAll ((,) <$> gi <*> gi)
+      i1 `union` i2 === (i2 `union` i1))
     ]
   , Group "interval"
     [ ("validity", property (forAll gi >>= assert . isValid))
