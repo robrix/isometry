849e508a2d4e0fdfd2eee298b1acbb96224f9d23
Rob Rix <rob.rix@me.com>
2020-07-10 08:20:19 -0400

    Revert "Define a utility for easier property definition."
    This reverts commit d026baf3111c4e73dd18f55ef24e3797a1615b39.

--- test/Test/Utils.hs
+++ test/Test/Utils.hs
@@ -1,9 +1,2 @@
 module Test.Utils
-( prop
-) where
-
-import GHC.Stack
-import Hedgehog
-
-prop :: HasCallStack => PropertyName -> PropertyT IO () -> (PropertyName, Property)
-prop name = (,) name . property
+() where
