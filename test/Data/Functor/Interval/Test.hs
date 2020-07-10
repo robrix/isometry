291825156cbad9ba50e07c0f62c077e04f43a58b
Rob Rix <rob.rix@me.com>
2020-07-10 08:02:11 -0400

    Test the transitivity of isSubintervalOf.

--- test/Data/Functor/Interval/Test.hs
+++ test/Data/Functor/Interval/Test.hs
@@ -23,6 +23,13 @@ tests = map checkParallel
     [ ("reflexivity", property $ do
       i <- forAll gi
       assert $ i `isSubintervalOf` i)
+    , ("transitivity", property $ do
+      i1 <- forAll gi
+      i2 <- forAll (superinterval i1)
+      i3 <- forAll (superinterval i2)
+      label $ (if i1 == i2 then "i1 = i2" else "i1 ⊂ i2") <> " ∧ " <> (if i2 == i3 then "i2 = i3" else "i2 ⊂ i3")
+      assert (i1 `isSubintervalOf` i3)
+      )
     ]
   , Group "isProperSubintervalOf"
     [ ("antireflexivity", property $ do
@@ -65,3 +72,11 @@ interval p = Gen.choice
   ]
   where
   mk a b = a ... a + b + 1
+
+superinterval :: (MonadGen m, Num a) => Interval I a -> m (Interval I a)
+superinterval i = do
+  l <- delta
+  r <- delta
+  pure $! Interval (inf i - fromIntegral l) (sup i + fromIntegral r)
+  where
+  delta = Gen.int (Range.linear 0 10)
