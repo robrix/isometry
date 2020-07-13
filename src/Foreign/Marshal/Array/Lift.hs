0c367b5941f74b97062c0c0fa6057b3d17cb35fc
Rob Rix <rob.rix@me.com>
2020-07-12 20:57:53 -0400

    Inline Foreign.Marshal.Array.Lift.

--- src/Foreign/Marshal/Array/Lift.hs
+++ src/Foreign/Marshal/Array/Lift.hs
@@ -14,24 +14,24 @@ import           Foreign.Storable
 
 mallocArray :: (Storable a, Has (Lift IO) sig m) => Int -> m (Ptr a)
 mallocArray = sendIO . A.mallocArray
-{-# INLINABLE mallocArray #-}
+{-# INLINE mallocArray #-}
 
 allocaArray :: (Has (Lift IO) sig m, Storable a) => Int -> (Ptr a -> m b) -> m b
 allocaArray n with = liftWith $ \ hdl ctx -> A.allocaArray n (hdl . (<$ ctx) . with)
-{-# INLINABLE allocaArray #-}
+{-# INLINE allocaArray #-}
 
 peekArray :: (Has (Lift IO) sig m, Storable a) => Int -> Ptr a -> m [a]
 peekArray n = sendM . A.peekArray n
-{-# INLINABLE peekArray #-}
+{-# INLINE peekArray #-}
 
 pokeArray :: (Has (Lift IO) sig m, Storable a) => Ptr a -> [a] -> m ()
 pokeArray p = sendM . A.pokeArray p
-{-# INLINABLE pokeArray #-}
+{-# INLINE pokeArray #-}
 
 withArray :: (Has (Lift IO) sig m, Storable a) => [a] -> (Ptr a -> m b) -> m b
 withArray as with = liftWith $ \ hdl ctx -> A.withArray as (hdl . (<$ ctx) . with)
-{-# INLINABLE withArray #-}
+{-# INLINE withArray #-}
 
 withArrayLen :: (Has (Lift IO) sig m, Storable a) => [a] -> (Int -> Ptr a -> m b) -> m b
 withArrayLen as with = liftWith $ \ hdl ctx -> A.withArrayLen as (\ n -> hdl . (<$ ctx) . with n)
-{-# INLINABLE withArrayLen #-}
+{-# INLINE withArrayLen #-}
