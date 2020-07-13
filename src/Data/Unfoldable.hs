75473606075a16d8b63d47c70b7f0e0bc938af1c
Rob Rix <rob.rix@me.com>
2020-07-12 21:04:06 -0400

    Inline Data.Unfoldable.

--- src/Data/Unfoldable.hs
+++ src/Data/Unfoldable.hs
@@ -16,8 +16,8 @@ class SparseUnfoldableWithIndex b i t | t -> b i where
   iunfoldSparseM :: Monad m => (b -> m Bool) -> (i -> m a) -> m (t a)
   iunfoldSparse :: (b -> Bool) -> (i -> a) -> t a
   iunfoldSparse branch (leaf :: i -> a) = coerce (iunfoldSparseM :: (b -> Identity Bool) -> (i -> Identity a) -> Identity (t a)) branch leaf
-  {-# INLINABLE iunfoldSparse #-}
+  {-# INLINE iunfoldSparse #-}
 
 tetra :: (Foldable v, SparseUnfoldableWithIndex (v Bit) i t) => (i -> a) -> t a
 tetra = iunfoldSparse (fromBit . foldl' xor B0)
-{-# INLINABLE tetra #-}
+{-# INLINE tetra #-}
