3981b354b69f771ce726214e8f04f104f1871f17
Rob Rix <rob.rix@me.com>
2020-07-12 20:57:45 -0400

    Inline GL.Buffer.

--- src/GL/Buffer.hs
+++ src/GL/Buffer.hs
@@ -45,10 +45,13 @@ newtype Buffer (ty :: Type) v = Buffer { unBuffer :: GLuint }
 
 instance Object (Buffer ty v) where
   gen = defaultGenWith glGenBuffers Buffer
+  {-# INLINE gen #-}
   delete = defaultDeleteWith glDeleteBuffers unBuffer
+  {-# INLINE delete #-}
 
 instance KnownType ty => Bind (Buffer ty v) where
   bind = checking . sendIO . glBindBuffer (glEnum (typeVal @ty)) . maybe 0 unBuffer
+  {-# INLINE bind #-}
 
 -- FIXME: Store the current size and don’t reallocate when larger.
 realloc :: forall ty v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has (Lift IO) sig m) => Int -> Update -> Usage -> m ()
@@ -56,35 +59,43 @@ realloc n update usage = sendIO (glBufferData (glEnum (typeVal @ty)) (fromIntegr
 
 copy :: forall ty v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m) => Int -> [v] -> m ()
 copy = copyWith @ty A.withArrayLen
+{-# INLINE copy #-}
 
 copyV :: forall ty v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m) => Int -> V.Vector v -> m ()
 copyV = copyWith @ty withVectorLen
+{-# INLINE copyV #-}
 
 withVectorLen :: (Has (Lift IO) sig m, Storable a) => V.Vector a -> (Int -> Ptr a -> m b) -> m b
 withVectorLen as with = liftWith $ \ hdl ctx -> V.unsafeWith as (hdl . (<$ ctx) . with (V.length as))
+{-# INLINE withVectorLen #-}
 
 copyMV :: forall ty v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m) => Int -> MV.IOVector v -> m ()
 copyMV = copyWith @ty withMutableVectorLen
+{-# INLINE copyMV #-}
 
 withMutableVectorLen :: (Has (Lift IO) sig m, Storable a) => MV.IOVector a -> (Int -> Ptr a -> m b) -> m b
 withMutableVectorLen as with = liftWith $ \ hdl ctx -> MV.unsafeWith as (hdl . (<$ ctx) . with (MV.length as))
+{-# INLINE withMutableVectorLen #-}
 
 copyA :: forall ty i v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m, Ix i) => Int -> StorableArray i v -> m ()
 copyA = copyWith @ty withStorableArrayLen
+{-# INLINE copyA #-}
 
 withStorableArrayLen :: (Has (Lift IO) sig m, Storable a, Ix i) => StorableArray i a -> (Int -> Ptr a -> m b) -> m b
 withStorableArrayLen as with = liftWith $ \ hdl ctx -> do
   bounds <- getBounds as
   withStorableArray as (hdl . (<$ ctx) . with (rangeSize bounds))
+{-# INLINE withStorableArrayLen #-}
 
 copyWith :: forall ty t v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m) => (t v -> (Int -> Ptr v -> m ()) -> m ()) -> Int -> t v -> m ()
 copyWith with offset vertices = with vertices (\ len -> copyPtr @ty ((0...len) + point (I offset)))
+{-# INLINE copyWith #-}
 
 copyPtr :: forall ty v m sig . (HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m, KnownType ty, S.Storable v, Has Check sig m, Has (Lift IO) sig m) => Interval I Int -> Ptr v -> m ()
 copyPtr i' ptr = do
   let i = i' ^* S.sizeOf @v undefined
   checking . sendIO . glBufferSubData (glEnum (typeVal @ty)) (fromIntegral (inf i)) (fromIntegral (diameter i)) $ castPtr ptr
-
+{-# INLINE copyPtr #-}
 
 
 data Type
@@ -98,18 +109,22 @@ class KnownType (ty :: Type) where
 
 instance KnownType 'Array where
   typeVal = Array
+  {-# INLINE typeVal #-}
 
 instance KnownType 'ElementArray where
   typeVal = ElementArray
+  {-# INLINE typeVal #-}
 
 instance KnownType 'Texture where
   typeVal = Texture
+  {-# INLINE typeVal #-}
 
 instance GL.Enum Type where
   glEnum = \case
     Array        -> GL_ARRAY_BUFFER
     ElementArray -> GL_ELEMENT_ARRAY_BUFFER
     Texture      -> GL_TEXTURE_BUFFER
+  {-# INLINE glEnum #-}
 
 
 data Update
@@ -137,6 +152,7 @@ instance GL.Enum Hint where
     Hint Stream  Draw -> GL_STREAM_DRAW
     Hint Stream  Read -> GL_STREAM_READ
     Hint Stream  Copy -> GL_STREAM_COPY
+  {-# INLINE glEnum #-}
 
 
 bindBuffer :: (KnownType ty, Has Check sig m, Has (Lift IO) sig m) => Buffer ty v -> BufferC ty v m a -> m a
@@ -144,8 +160,10 @@ bindBuffer buffer m = do
   bind (Just buffer)
   a <- runReader buffer (runLabelled m)
   a <$ bind (Nothing `asTypeOf` Just buffer)
+{-# INLINE bindBuffer #-}
 
 askBuffer :: forall ty v m sig . HasLabelled (Buffer ty) (Reader (Buffer ty v)) sig m => m (Buffer ty v)
 askBuffer = runUnderLabel @(Buffer ty) ask
+{-# INLINE askBuffer #-}
 
 type BufferC ty v = Labelled (Buffer ty) (ReaderC (Buffer ty v))
