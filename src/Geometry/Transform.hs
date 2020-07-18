ff97aa0a617e6dcd143d4fcdc31136f3e4b355fc
Rob Rix <rob.rix@me.com>
2020-07-18 07:25:22 -0400

    Define transformation of vectors in the opposite direction.

--- src/Geometry/Transform.hs
+++ src/Geometry/Transform.hs
@@ -11,6 +11,7 @@ module Geometry.Transform
 , mkScale
 , mkRotation
 , apply
+, apply'
 , tmap
 , identity
 , (>>>)
@@ -60,6 +61,9 @@ mkRotation q = Transform (L.identity !*! mkTransformation (coerce q) 0)
 
 apply :: (Num c, Unit d a, Unit d b, Additive m, Foldable m) => Transform m c a b -> m (a c) -> m (b c)
 apply (Transform m) v = pure <$> (m !* fmap prj v)
+
+apply' :: (Num c, Unit d a, Unit d b, Additive m, Foldable m) => Transform m c a b -> m (b c) -> m (a c)
+apply' (Transform m) v = pure <$> (fmap prj v *! m)
 
 tmap :: Functor m => (c -> c') -> Transform m c a b -> Transform m c' a b
 tmap f = Transform . fmap (fmap f) . getTransform
