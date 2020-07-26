4cb88fb6a51edf7af97a11c1ebf83005b32e829b
Rob Rix <rob.rix@me.com>
2020-07-26 15:47:45 -0400

    Define a KnownN class reifying an N.

--- src/Data/Nat.hs
+++ src/Data/Nat.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 module Data.Nat
 ( N(..)
+, KnownN(..)
 ) where
 
 data N
   = Z
   | S N
   deriving (Eq, Ord, Show)
+
+class KnownN (n :: N) where
+  reifyN :: N
