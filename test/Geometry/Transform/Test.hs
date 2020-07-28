2f37edf77a842cb089aa9e1dc9fba9da1fe6959a
Rob Rix <rob.rix@me.com>
2020-07-28 10:10:52 -0400

    Test apply against the identity transform.

--- test/Geometry/Transform/Test.hs
+++ test/Geometry/Transform/Test.hs
@@ -1,9 +1,34 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Geometry.Transform.Test
 ( tests
 ) where
 
-import Hedgehog
+import           Data.Functor.I
+import           Geometry.Transform
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import           Linear.Exts hiding (identity)
+import           Unit
+
+prop_apply_identity = property $ do
+  vec <- forAll (v4 u)
+  identity `apply` vec === vec
+
+
+u :: MonadGen m => m (U Int)
+u = U <$> Gen.int (Range.linear 0 100)
+
+v4 :: (MonadGen m, Num a) => m a -> m (V4 a)
+v4 g = V4 <$> g <*> g <*> g <*> pure 1
+
+
+newtype U a = U { getU :: a }
+  deriving (Eq, Floating, Fractional, Num, Ord, Real, Show)
+  deriving (Applicative, Functor, Unit I) via I
+
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
