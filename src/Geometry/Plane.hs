ec6a7780a892e9f5dcd5fd1063e6e60cfea64c1f
Rob Rix <rob.rix@me.com>
2020-08-30 11:55:27 -0400

    Compute the signed distance between a plane and a point.

--- src/Geometry/Plane.hs
+++ src/Geometry/Plane.hs
@@ -1,2 +1,9 @@
 module Geometry.Plane
-() where
+( signedDistance
+) where
+
+import Linear.Metric
+import Linear.Vector
+
+signedDistance :: (Metric v, Num a) => v a -> v a -> v a -> a
+signedDistance o n p = (p ^-^ o) `dot` n
