4659e9ff90ea3d8986e5e9e7e3a31e6ab3abd08b
Rob Rix <rob.rix@me.com>
2020-09-01 20:13:11 -0400

    Test that the signed distance to the plane is 0.

--- test/Geometry/Plane/Test.hs
+++ test/Geometry/Plane/Test.hs
@@ -3,7 +3,27 @@ module Geometry.Plane.Test
 ( tests
 ) where
 
-import Hedgehog
+import           Geometry.Plane
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import           Linear.V2
+
+prop_signedDistance_identity = property $ do
+  v <- forAll $ v2 coord
+  n <- forAll (v2 (nonZero coord))
+  signedDistance v n v === 0
+
+
+coord :: MonadGen m => m Rational
+coord = Gen.realFrac_ (Range.linearFrac (-100) 100)
+
+nonZero :: (MonadGen m, Num a, Eq a) => m a -> m a
+nonZero = Gen.filterT (/= 0)
+
+v2 :: MonadGen m => m a -> m (V2 a)
+v2 g = V2 <$> g <*> g
+
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
